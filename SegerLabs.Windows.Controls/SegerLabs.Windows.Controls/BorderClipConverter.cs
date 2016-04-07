b10609a284e72f6f2c605c8c75cea5d6c1c8ac06
Jesse Seger <jesses@hawkridgesys.com>
2016-04-07 12:06:16 -0400

    had to add converter

---
+++ SegerLabs.Windows.Controls/SegerLabs.Windows.Controls/BorderClipConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SegerLabs.Windows.Controls
+{
+    public class BorderClipConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 3 && values[0] is double && values[1] is double && values[2] is CornerRadius) {
+                var width = (double)values[0];
+                var height = (double)values[1];
+
+                if (width < Double.Epsilon || height < Double.Epsilon) {
+                    return Geometry.Empty;
+                }
+
+                var radius = (CornerRadius)values[2];
+
+                var clip = new RectangleGeometry(new Rect(0, 0, width, height), radius.TopLeft, radius.TopLeft);
+                clip.Freeze();
+
+                return clip;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
